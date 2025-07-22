2a84b7d4b79997ce694885d1f58a62d9f1fa8809
vini <viniciusmg@id.uff.br>
2025-07-22 01:09:41 -0300

    Fix dependency issues for deployment

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="whisper-notebook",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "flask>=3.1.1",
+        "gunicorn>=23.0.0",
+        "openai-whisper>=20240930",
+        "numpy>=1.26.4",
+        "torch>=2.7.1",
+        "tqdm>=4.67.1",
+        "ffmpeg-python"
+    ],
+)
